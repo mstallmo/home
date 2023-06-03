fee09a0af47b70b38d17e6e575ac8c577942eafc
Mason Stallmo <masonstallmo@gmail.com>
2023-06-03 10:49:12 -0700

    Use turbo frame to get SPA effect

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#show"
+  root "dashboard#show"
+  get "teams", to: "teams#index"
 end
